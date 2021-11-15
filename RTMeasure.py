9332ddcf49b2b88b44a10b4f0d4e3c71a250b08a
Clayden Boyd <71368259+clayboyd06@users.noreply.github.com>
2021-11-15 15:58:44 -0800

    Add files via upload

--- RTMeasure.py
+++ RTMeasure.py
@@ -20,9 +20,10 @@ cap = cv2.VideoCapture(0)
 cap.set(10, 160)
 cap.set(3, 1920)
 cap.set(4, 1080)
-scale = 3
-wP = 210*scale
-hP = 297*scale
+
+pixel_scale = 3 #TODO = D / ref_width = 5 microns - establish D with a calibration frame as numb pixels.
+                ## Get top bottom and 
+
 
 
 ########## end Settings ############
@@ -34,9 +35,20 @@ while(1):
     img, cts = utils.getConts(img, draw=True)
     if len(cts) !=0 :
         biggest = cts[0][2]
-        print(biggest)
-        #imgWarp = utils.imgWarp(img, biggest, wP, hP)
-        #cv2.imshow("warped", imgWarp)
+        top, bottom = utils.top_bottom(img, cts)
+        
+        D = utils.distance(top, bottom)
+        mx, my = utils.midpoint(top, bottom)
+        # if(calibrate):px_per_dist = D
+        #if px_per_dist is not none: distance = D / px_per_dist
+        #print("Top: {} Bottom: {} Distance: {} ".format(top, bottom, D / pixel_scale))
+
+        #Show distance on monitor
+        cv2.circle(img, (int(top[0]), int(top[1])), 5, (0, 0, 255), -1)
+        cv2.circle(img, (int(bottom[0]), int(bottom[1])), 5, (0, 0, 255), -1)
+        cv2.line(img, (int(top[0]), int(top[1])), (int(bottom[0]), int(bottom[1])), (0, 0, 255), 2)
+        cv2.putText(img, " {:.1f} um".format(D), (int(my), int(my - 10)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
         
     img = cv2.resize(img, (0,0), None, 0.5, 0.5)
     
--- utils.py
+++ utils.py
@@ -1,9 +1,27 @@
 import cv2
+from scipy.spatial import distance as dist
 import numpy as np
 
+'''
+    getConts uses canny edge detection to get the contours of an image.
+
+    input parameters:
+        img the cv2 frame
+        cThr - the threshold values for canny. Defaults are [100, 150]
+        showCanny - will open a window to show the canny edge detection. False by default
+        minArea - the minium area of an object to be returned. Default is 1000
+        filter - filters the number of points on an object edge. 0 by default
+        draw - boolean to control if the bounding box is drawn on the frame - False by default
+    
+    returns the image data and an array called finalContours which contains the data of the size
+    of a contour, the area, the approximation of the points, the bounding box, the min and max points, and
+    the raw data.
+    
+'''
 def getConts(img, cThr = [100,150], showCanny=False, minArea=1000, filter =0, draw=False):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (5,5), 1)
+    #thresh = cv2.threshold(imgBlur, 220, 255, cv2.THRESH_BINARY_INV)[1]
     edges = cv2.Canny(imgBlur, cThr[0], cThr[1])
     kernal = np.ones((5,5))
     dilated = cv2.dilate(edges, kernal, iterations=3)
@@ -15,30 +33,66 @@ def getConts(img, cThr = [100,150], showCanny=False, minArea=1000, filter =0, dr
     for c in cnts:
         area = cv2.contourArea(c)
         if area > minArea:
-            print("here!")
             peri = cv2.arcLength(c, True)
             approx = cv2.approxPolyDP(c, 0.02*peri, True)
             bbox = cv2.boundingRect(approx)
-
+            miny = tuple(c[c[:, :, 1].argmax()][0])
+            maxy = tuple(c[c[:, :, 1].argmin()][0])
+            #print(miny, maxy)
+        
             if (filter > 0):
                 if len(approx) == filter:
-                    finalContours.append((len(approx), area, approx, bbox, c))
-                    print("boop")
+                    finalContours.append((len(approx), area, approx, bbox, miny, maxy, c))
             else:
-                finalContours.append((len(approx), area, approx, bbox, c))
-                print("blipp")
+                finalContours.append((len(approx), area, approx, bbox, miny, maxy, c))
 
     finalContours = sorted(finalContours, key = lambda x:x[1] , reverse = True)
 
     if draw:
         for con in finalContours:
-            cv2.drawContours(img, con[4], -1, (0,0,255), 3)
+            cv2.drawContours(img, con[6], -1, (0,0,255), 3)
 
     return img, finalContours
 
-## reorders points to follow the order:
-## TL, BL, TR, BR
+
+def distance(a, b):
+'''
+    Returns the distance in pixels
+'''
+    return dist.euclidean((a[0], a[1]), (b[0], b[1]))
+
+
+def midpoint (a, b):
+'''
+    Returns the midway point 
+'''
+    return (0.5*(a[0]+b[0]), 0.5*(a[1]+b[1]))
+
+def top_bottom(img, conts):
+'''
+    returns the (x,y) coordinates of the top point of the bottom object
+    and the bottom point of the top object
+'''
+    maxy = 0
+    miny = np.inf
+    top = 0
+    bottom =0
+    for i in range(len(conts)):
+        if conts[i][4][1] < miny:
+            miny = conts[i][4][1]
+            bottom = conts[i][4]
+        if conts[i][5][1] > maxy:
+            maxy = conts[i][5][1]
+            top = conts[i][5]
+    return top, bottom
+    
+
+
 def reorder(points):
+'''
+    reorders points to follow the order:
+     TL, BL, TR, BR
+'''
     newPoints = np.zeros_like(points)
     points = points.reshape((4,2))
     
@@ -52,7 +106,16 @@ def reorder(points):
     return newPoints
 
 def imgWarp(img, points, w, h, pad=20):
-    #points = reorder(points)
+''' 
+    returns the frame warped to fit a given width and height
+    parmeters:
+        img - the frame to be warped
+        points - the corner points of the image to be shifted
+        w - the desired width
+        h - desired height
+        pad - the edge buffer to fit the full monitor, default = 20
+'''
+    points = reorder(points)
     pts1 = np.float32(points)
     pts2 = np.float32([[0,0],[w,0],[0,h],[w,h]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
